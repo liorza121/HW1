7cc657a22a75ec486febb23d74ace4b9cf837073
viper010404 <ido.falah@campus.technion.ac.il>
2023-05-14 17:05:02 +0300

    classes done

--- HW1/src/Board.java
+++ HW1/src/Board.java
@@ -2,6 +2,39 @@ import java.util.Arrays;
 
 public class Board {
 
+    private Tile tiles[][];
+    private final int size;
+
+    public Board(String tileList){
+        this.size = getSize(tileList);
+        tiles = new Tile[size][size];
+        String value;
+        for(int i = 0; i < (size*size)*2; i += 2){
+            value = tileList.substring(i, i+1);
+            if (value.equals("_")){
+                tiles[i / (size*2)][(i/2)%size] = null;
+            }
+            else {
+                tiles[i / (size*2)][(i / 2) % size] = new Tile(Integer.parseInt(value));
+            }
+        }
+    }
+
+    @Override
+    public String toString(){
+        String ret = "";
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(tiles[i][j] == null)
+                    ret += "_|";
+                else
+                    ret += tiles[i][j] + "|";
+            }
+            ret += "\n";
+        }
+        return ret;
+    }
+
     @Override
     public boolean equals(Object other) {
         if (!(other instanceof Board)) {
@@ -14,5 +47,15 @@ public class Board {
     @Override
     public int hashCode() {
         return Arrays.deepHashCode(tiles);
+    }
+
+    private int getSize(String tileList){
+        int count = 0;
+        for(int i = 0; i < tileList.length(); i++){
+            if (tileList.charAt(i) == '|'){
+                count++;
+            }
+        }
+        return count + 1;
     }
 }
